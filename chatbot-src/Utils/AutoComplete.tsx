a0656ae1431c03bccfdc7dd887e70829246a7c5e
JiSungE <hyt970@gmail.com>
2023-01-17 16:29:28 +0900

    채팅페이지에 하루동안 보지않기 추가

--- chatbot-src/Utils/AutoComplete.tsx
+++ chatbot-src/Utils/AutoComplete.tsx
@@ -2,6 +2,10 @@
 /* eslint-disable semi */
 
 /* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-unreachable */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, {useEffect, useState} from 'react';
 import {
   ScrollView,
