b0e3b120b385487f32bb0d7ccafde56671b71922
yeomwon0 <one6727@naver.com>
2023-01-17 20:33:44 +0900

    20230117

--- chatbot-src/ChatForm/BtnSystemChat.tsx
+++ chatbot-src/ChatForm/BtnSystemChat.tsx
@@ -8,18 +8,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, Image, Text, TouchableOpacity, View} from 'react-native';
-import { setTemp } from '../../MapForm/K_Map/S';
-import { setPostData } from '../Chatting';
+import {
+  ActivityIndicator,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {setTemp} from '../../MapForm/K_Map/KMap';
 import {SystemTime} from '../Utils/SytemTime';
 
-export const BtnSystemChat = ({keyword,answer}:any)=>{
-    useEffect(()=>{
-      setPostData(keyword);
-      setTemp(keyword);
-    },[]);
-    return (
-        <>
+export const BtnSystemChat = ({keyword, answer}: any) => {
+  useEffect(() => {
+    setTemp(keyword);
+  }, []);
+  return (
+    <>
       <View
         style={{
           paddingHorizontal: 10,
@@ -32,25 +36,23 @@ export const BtnSystemChat = ({keyword,answer}:any)=>{
         <Text style={{fontSize: 20, marginTop: 50}}>보리</Text>
       </View>
       <TouchableOpacity
-            activeOpacity = {1}
-            style={{
-              borderTopRightRadius: 30,
-              borderBottomLeftRadius: 30,
-              borderBottomRightRadius: 30,
-              backgroundColor: 'white',
-              margin: 5,
-              borderWidth: 1,
-              borderColor: '#c7c7c7',
-              shadowOpacity: 0.75,
-              shadowRadius: 3.84,
-              elevation: 5,
-              marginHorizontal: 40,
-              marginLeft: 30,
-            }
-          }
-      >
-        <View
+        activeOpacity={1}
         style={{
+          borderTopRightRadius: 30,
+          borderBottomLeftRadius: 30,
+          borderBottomRightRadius: 30,
+          backgroundColor: 'white',
+          margin: 5,
+          borderWidth: 1,
+          borderColor: '#c7c7c7',
+          shadowOpacity: 0.75,
+          shadowRadius: 3.84,
+          elevation: 5,
+          marginHorizontal: 40,
+          marginLeft: 30,
+        }}>
+        <View
+          style={{
             paddingVertical: 15,
             paddingHorizontal: 10,
             borderColor: 'black',
@@ -58,32 +60,32 @@ export const BtnSystemChat = ({keyword,answer}:any)=>{
             borderTopRightRadius: 30,
             borderBottomLeftRadius: 30,
             borderBottomRightRadius: 30,
-          }}>        
-        <Text style={{fontSize:15,lineHeight:24, color: 'black'}}>
-          {answer}
-        </Text>
-        <TouchableOpacity style={{
-          backgroundColor: 'white',
-          borderRadius: 20,
-          borderColor: '#544fc1',
-          borderWidth: 2,
-          marginTop: 30,
-          paddingTop: 5,
-          paddingBottom: 5,
-        }}>
-          <Text 
-          style={{
-            alignItems: 'center',
-            textAlign: 'center',
-            color: '#544fc1',
-          }}
-          >
-            지도로 가기
+          }}>
+          <Text style={{fontSize: 15, lineHeight: 24, color: 'black'}}>
+            {answer}
           </Text>
-        </TouchableOpacity>
-    </View>
+          <TouchableOpacity
+            style={{
+              backgroundColor: 'white',
+              borderRadius: 20,
+              borderColor: '#544fc1',
+              borderWidth: 2,
+              marginTop: 30,
+              paddingTop: 5,
+              paddingBottom: 5,
+            }}>
+            <Text
+              style={{
+                alignItems: 'center',
+                textAlign: 'center',
+                color: '#544fc1',
+              }}>
+              지도로 가기
+            </Text>
+          </TouchableOpacity>
+        </View>
       </TouchableOpacity>
       <SystemTime />
-        </>
-      );
+    </>
+  );
 };
